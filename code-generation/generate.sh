d95e3516674bb583c6ff8a304d2b013b392eb281
sinapordanesh <sina.pordanesh@yahoo.com>
2024-08-17 14:06:25 -0400

    DRA cluster configuration and setup

--- code-generation/generate.sh
+++ code-generation/generate.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 # ---------------------------------------------------------------------
-# Script to process any file for temporary work
+# Script to process any file for temporary work on Digital Research Alliance Canada
 # ---------------------------------------------------------------------
-#SBATCH --job-name=temp-run
+#SBATCH --job-name=code-generation-run
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
+#SBATCH --gpus-per-node=a100:2
 #SBATCH --mem=256gb
-#SBATCH --time=0-24:00:00
-#SBATCH --partition=bigmem
+#SBATCH --time=0-110:00:00
 
-#SBATCH --partition=gpu-a100
-#SBATCH --gres=gpu:2  # Request 2 GPUs
+# Optional: Email notifications
+#SBATCH --mail-user=myhomeqrc@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
 # ---------------------------------------------------------------------
 echo "Starting run at: `date`"
 # ---------------------------------------------------------------------
-# module load python/3.9.7
-source ~/software/init-conda
-conda activate myenv
-python generate.py --path data/main_data/reformatted_prompts --model_name codeparrot/codeparrot
+source ../env/bin/activate
+python generate.py --path data/CodeSearchNet --model_name codellama/CodeLlama-7b-hf --max_num 100000
 # ---------------------------------------------------------------------
 echo "Job finished with exit code $? at: `date`"
 # ---------------------------------------------------------------------
 
 
-# USAGE:
-# $sbatch your-file.slum
-# $squeue -u your_username
-# $squeue -j jobID
-# $scontrol show job jobID
-# $squeue -p partition_name
-
-# current_id: squeue -j 31789681
+# current_id: squeue -j 33101314
